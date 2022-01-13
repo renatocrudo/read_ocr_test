fb4f09c355a49d0ebff4e1cec6789d385570cb4d
Renato Crudo <re.crudo.85@gmail.com>
2022-01-13 13:48:22 -0300

    Read image

--- src/lendo_images.py
+++ src/lendo_images.py
@@ -2,9 +2,10 @@ import pytesseract
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import base64
 
 img = cv2.imread("images/cupom-fiscal-1.jpg")
-print(img)
+#print(img)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 #cv2.imshow('Cute Kitens', img)
@@ -15,3 +16,22 @@ plt.figure(figsize=(20,10))
 
 extrair = pytesseract.image_to_string(img, lang="por")
 print(extrair)
+
+def save_image(image):
+    img_bytes = bytearray(image.encode())
+    with open("images/recibo.jpg", "wb") as fh:
+        fh.write(base64.decodebytes(img_bytes))
+        #fh.write(image)
+
+def read_image(id, image):
+    save_image(image)
+    imagem = cv2.imread("images/recibo.jpg")
+    imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+    #imagem_bytes = base64.b64decode(image)
+    #im_arr = np.frombuffer(imagem_bytes, dtype=np.uint8)
+    #print(im_arr)
+    #imagem = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+    #imagem = cv2.cvtColor(im_arr, cv2.COLOR_BGR2RGB)
+    texto = pytesseract.image_to_string(imagem, lang="por")
+    print(texto)
+    return texto
--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from flask import Flask, json, jsonify, request
-
+from flask_pydantic_spec import FlaskPydanticSpec
+import lendo_images
 
 app = Flask(__name__)
+spec = FlaskPydanticSpec('flask', title='Read OCR API')
+spec.register(app)
+
+@app.route("/read_ocr", methods=["POST"])
+def read_ocr():
+    recibo_details = request.get_json()
+    imagem = recibo_details["foto_recibo"]
+    #print(imagem)
+    resultado = lendo_images.read_image(1, imagem)
+    print(resultado)
+
+    return jsonify(resultado)
 
 if __name__ == "__main__":
-    create_tables()
-    app.run()
+
+    app.run(port=6000)
